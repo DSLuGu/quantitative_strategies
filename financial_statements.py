0795d65c7f76132027371c4db23a67cc5952972f
DSLuGu <ds.lminho248@gmail.com>
2022-05-04 20:35:30 +0900

    Update financial_statements.py

--- financial_statements.py
+++ financial_statements.py
@@ -33,7 +33,91 @@ def get_bs(df):
         else:
             newColumns.append(i[1])
     
-    fsBS.columns = newColumns
+    df.columns = newColumns
+    df = df[[c for c in newColumns if not re.search(r'class\d', c)]]
+    
+    years = [c for c in df.columns if re.search(r'\d{8}', c)][::-1]
+    nonYears = [c for c in df.columns if not re.search(r'\d{8}', c)]
+    
+    rtnDF = pd.DataFrame()
+    for y in years:
+        appendDF = deepcopy(df[nonYears+[y]]).rename(columns={f'{y}': 'value'})
+        appendDF['year'] = y
+        rtnDF = pd.concat([rtnDF, appendDF])
+    
+    rtnDF = rtnDF[['year']+nonYears+['value']]
+    
+    return rtnDF
+
+
+def get_is(df):
+    
+    if len(df) == 0: return None
+    
+    newColumns = []
+    for i in df.columns:
+        if re.search(r'\d{8}\-\d{8}', i[0]):
+            newColumns.append(i[0][-8:])
+        else:
+            newColumns.append(i[1])
+    
+    df.columns = newColumns
+    df = df[[c for c in newColumns if not re.search(r'class\d', c)]]
+    
+    years = [c for c in df.columns if re.search(r'\d{8}', c)][::-1]
+    nonYears = [c for c in df.columns if not re.search(r'\d{8}', c)]
+    
+    rtnDF = pd.DataFrame()
+    for y in years:
+        appendDF = deepcopy(df[nonYears+[y]]).rename(columns={f'{y}': 'value'})
+        appendDF['year'] = y
+        rtnDF = pd.concat([rtnDF, appendDF])
+    
+    rtnDF = rtnDF[['year']+nonYears+['value']]
+    
+    return rtnDF
+
+
+def get_cis(df):
+    
+    if len(df) == 0: return None
+    
+    newColumns = []
+    for i in df.columns:
+        if re.search(r'\d{8}\-\d{8}', i[0]):
+            newColumns.append(i[0][-8:])
+        else:
+            newColumns.append(i[1])
+    
+    df.columns = newColumns
+    df = df[[c for c in newColumns if not re.search(r'class\d', c)]]
+    
+    years = [c for c in df.columns if re.search(r'\d{8}', c)][::-1]
+    nonYears = [c for c in df.columns if not re.search(r'\d{8}', c)]
+    
+    rtnDF = pd.DataFrame()
+    for y in years:
+        appendDF = deepcopy(df[nonYears+[y]]).rename(columns={f'{y}': 'value'})
+        appendDF['year'] = y
+        rtnDF = pd.concat([rtnDF, appendDF])
+    
+    rtnDF = rtnDF[['year']+nonYears+['value']]
+    
+    return rtnDF
+
+
+def get_cf(df):
+    
+    if len(df) == 0: return None
+    
+    newColumns = []
+    for i in df.columns:
+        if re.search(r'\d{8}\-\d{8}', i[0]):
+            newColumns.append(i[0][-8:])
+        else:
+            newColumns.append(i[1])
+    
+    df.columns = newColumns
     df = df[[c for c in newColumns if not re.search(r'class\d', c)]]
     
     years = [c for c in df.columns if re.search(r'\d{8}', c)][::-1]
@@ -55,21 +139,38 @@ def main():
     dart.set_api_key(api_key=get_dart_api_key())
     
     corpList = dart.get_corp_list()
-    
-    # include_delisting(상장폐지 주식 포함 검색)
-    corp = corpList.find_by_stock_code(stock_code='005930', include_delisting=True)
-    corpInfo = corp.load()
-    fs = corp.extract_fs(bgn_de=corpInfo['est_dt'])
-    
-    fsBS = fs.show('bs')
-    # bsLabels = fs.labels['bs']
-    fsIS = fs.show('is')
-    # isLabels = fs.labels['is']
-    fsCIS = fs.show('cis')
-    # cisLabels = fs.labels['cis']
-    
-    get_bs(fsBS)
-    
+    for corp in corpList[90000:]:
+        corpInfo = corp.load()
+        print(corpInfo)
+        
+        try:
+            fs = corp.extract_fs(bgn_de=corpInfo['est_dt'])
+        except RuntimeError as e:
+            print(f"[{corpInfo['corp_code']}]{corpInfo['corp_name']}", e)
+            continue
+        
+        fsBS = fs.show('bs')
+        bsLabels = fs.labels['bs']
+        fsBS.to_excel(f"{corpInfo['corp_code']}_before_bs.xlsx", index=False)
+        fsIS = fs.show('is')
+        isLabels = fs.labels['is']
+        fsIS.to_excel(f"{corpInfo['corp_code']}before_is.xlsx", index=False)
+        fsCIS = fs.show('cis')
+        cisLabels = fs.labels['cis']
+        fsCIS.to_excel(f"{corpInfo['corp_code']}before_cis.xlsx", index=False)
+        fsCF = fs.show('cf')
+        cfLabels = fs.labels['cf']
+        fsCF.to_excel(f"{corpInfo['corp_code']}before_cf.xlsx", index=False)
+        
+        bsDF = get_bs(fsBS)
+        bsDF.to_excel(f"{corpInfo['corp_code']}after_bs.xlsx", index=False)
+        isDF = get_is(fsIS)
+        isDF.to_excel(f"{corpInfo['corp_code']}after_is.xlsx", index=False)
+        cisDF = get_cis(fsCIS)
+        cisDF.to_excel(f"{corpInfo['corp_code']}after_cis.xlsx", index=False)
+        cfDF = get_cf(fsCF)
+        cfDF.to_excel(f"{corpInfo['corp_code']}after_df.xlsx", index=False)
+        
     return None
 
 
